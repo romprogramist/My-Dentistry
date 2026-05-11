1b70cb88284d9120301c8ff8a8a5ba7cf4c2f704
Roman <romprogramist@gmail.com>
2026-05-11 21:43:26 +0300

    test(layout): assert MobileStickyCTA buttons get utility classes from buttonVariants

--- components/layout/MobileStickyCTA.test.tsx
+++ components/layout/MobileStickyCTA.test.tsx
@@ -29,4 +29,14 @@ describe("MobileStickyCTA", () => {
     const { container } = render(<MobileStickyCTA />);
     expect(container.firstChild).toHaveClass("md:hidden");
   });
+
+  test("buttons receive flex-1 and min-h-11 utility classes via buttonVariants", () => {
+    render(<MobileStickyCTA />);
+    const call = screen.getByRole("link", { name: /позвонить/i });
+    const book = screen.getByRole("link", { name: /записаться/i });
+    expect(call.className).toMatch(/\bflex-1\b/);
+    expect(call.className).toMatch(/\bmin-h-11\b/);
+    expect(book.className).toMatch(/\bflex-1\b/);
+    expect(book.className).toMatch(/\bmin-h-11\b/);
+  });
 });
